cdf5bc2b655ff0dae0a93f69f68e68d951404c55
dval <dylan.valentine@gmail.com>
2019-10-22 17:09:08 -0400

    Multi-plots now labeled correctly

--- HWiNFO64 parsing.R
+++ HWiNFO64 parsing.R
@@ -12,6 +12,9 @@ nL = countLines(csvPath)
 # as numeric.
 nC = 214
 
+rowStart = 2500
+rowEnd = 2700
+
 datasample <- read.csv(file=csvPath, 
                        header=TRUE, 
                        stringsAsFactors=FALSE, 
@@ -26,17 +29,26 @@ options( digits=3)
 
 cpuL <- as.data.frame(datasample[c(1:(nL-3)), c(grep("CPU", colnames(datasample)) )])
 
-gpuL <- as.data.frame(datasample[c(1:(nL-3)), c(grep('^GPU.*C\\]', colnames(datasample)) )])
+gpuL <- as.data.frame(datasample[c(rowStart:(nL-rowEnd)), c(grep('^GPU.*C\\]', colnames(datasample)) )])
 
-gpuL$Time = as.POSIXct(strptime(paste(datasample$Date, datasample$Time), "%d.%m.%Y %H:%M:%OS"))
+gpuL$Time = as.POSIXct(strptime(paste(datasample[c(rowStart:(nL-rowEnd)),]$Date, datasample[c(rowStart:(nL-rowEnd)),]$Time), "%d.%m.%Y %H:%M:%OS"))
 options(digits.secs=3)
 
+df <- gpuL %>%
+  select(colnames(gpuL)) %>%
+  gather(key = "variable", value = "value", -Time)
+head(df)
 #gputemp$`GPU Temperature [°C]` <- as.numeric(as.character(gputemp$`GPU Temperature [°C]`))
+ggplot(df, aes(x = Time, y = value)) + 
+  geom_line(aes(colour = variable)) + 
+  labs(y="Tempurature", colour="Measurement")
 
-ggplot(gpuL, aes(x=Time)) + 
-  geom_hline(yintercept = 60) + 
-  labs(y="Tempurature") + 
-  geom_line(aes( y=gpuL[[1]], colour="red")) + 
-  geom_line(aes( y=gpuL[[2]], colour="blue")) + 
-  geom_line(aes( y=gpuL[[3]], colour="grey")) 
+# ggplot(gpuL, aes(x=Time)) + 
+#   geom_hline(yintercept = 60) + 
+#   labs(y="Tempurature") + 
+#   geom_line(aes( y=gpuL[[1]], colour="red")) + 
+#   geom_line(aes( y=gpuL[[2]], colour="blue")) + 
+#   geom_line(aes( y=gpuL[[3]], colour="grey")) 
+
+
 
